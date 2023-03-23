5421e4cb4ea9a4a0014b6c11ddfcb693de1e8ae7
Rattapon Simsri <rattapon.simsri0@gmail.com>
2023-03-23 13:04:04 +0700

    REFACTOR : initial driver

--- libs/storage/src/module/service.ts
+++ libs/storage/src/module/service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { StorageDriver } from '../interface/storageDriver.interface';
 import {
   DiskOptions,
@@ -10,13 +10,21 @@ import { MODULE_OPTIONS_TOKEN } from '../utils/storage.module-definition';
 @Injectable()
 export class StorageService {
   private drivers = new Map<string, StorageDriver>();
+  private logger = new Logger();
 
   constructor(
     @Inject(MODULE_OPTIONS_TOKEN) private readonly options: StorageOptions,
   ) {
+    this.inintDriver(options);
+  }
+
+  private inintDriver(options: StorageOptions) {
     const disks = Object.entries(this.options.disks);
     disks.forEach((val) => {
       this.drivers.set(val[0], this.newDriver(val[1]));
+      this.logger.log(
+        `Storage service create driver ${val[1].driver} on disk ${val[0]} successfully`,
+      );
     });
   }
 
