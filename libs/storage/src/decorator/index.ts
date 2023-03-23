27c08b8c282809a464f40bade9b169b033788f47
Rattapon Simsri <rattapon.simsri0@gmail.com>
2023-03-23 15:36:52 +0700

    DELETE : decorator

--- libs/storage/src/decorator/index.ts
+++
@@ -1,8 +0,0 @@
-import { Inject } from '@nestjs/common';
-
-const getDriverName = (name: string) => {
-  return name;
-};
-
-export const InjectStorage = (name: string): ParameterDecorator =>
-  Inject(getDriverName(name));
