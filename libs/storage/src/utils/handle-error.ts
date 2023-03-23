6e6a99f52970f5e6e945acb3d7747101daf23f9f
Rattapon Simsri <rattapon.simsri0@gmail.com>
2023-03-23 15:22:10 +0700

    REFACTOR : rename func handle error

--- libs/storage/src/utils/handle-error.ts
+++ libs/storage/src/utils/handle-error.ts
@@ -1,6 +1,6 @@
 import { Logger } from '@nestjs/common';
 
-export const failedOrError = (message: string, err: any) => {
+export const failOrError = (message: string, err: any) => {
   const logger = new Logger();
   logger.error(`${err}  :: ${message}`);
   logger.error(err);
